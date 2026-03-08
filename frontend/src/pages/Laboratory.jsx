6cc9707f6be3bae848c34aa8560002eff609ebb1
LEONHEIL <leozcordz@gmail.com>
2026-03-08 11:21:26 +0800

    Created laboratory page

---
+++ frontend/src/pages/Laboratory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Laboratory = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold">Laboratory</h2>
+      <p className="mt-4">Welcome to laboratory page.</p>
+    </div>
+  );
+};
+
+export default Laboratory;
