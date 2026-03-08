c82cf505d96a2e4f670a2e79219bc37bcf746d15
LEONHEIL <leozcordz@gmail.com>
2026-03-08 11:20:45 +0800

    Created computer page

---
+++ frontend/src/pages/Computer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Computer = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold">Computer</h2>
+      <p className="mt-4">Welcome to computer page.</p>
+    </div>
+  );
+};
+
+export default Computer;
