9410273ccd25c27403afaa989e0b17cbe07e43c4
LEONHEIL <leozcordz@gmail.com>
2026-03-17 21:01:17 +0800

    Created post router for inventory

--- backend/src/routes/inventoryRoutes.js
+++ backend/src/routes/inventoryRoutes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   fetchInventory,
   fetchById,
+  createInventory,
 } from "../controllers/inventoryController.js";
 
 const router = Router();
 
 router.get("/inventory", fetchInventory);
 router.get("/inventory/:id", fetchById);
+router.post("/inventory/", createInventory);
 
 export default router;
