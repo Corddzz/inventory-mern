fe67af78f9e39e7bfdc82669830951269ed36c70
LEONHEIL <leozcordz@gmail.com>
2026-03-17 21:00:22 +0800

    Created insert controller function

--- backend/src/controllers/inventoryController.js
+++ backend/src/controllers/inventoryController.js
@@ -1,4 +1,8 @@
-import { getAllInventory, getById } from "../models/Inventory.js";
+import {
+  getAllInventory,
+  getById,
+  insertInventory,
+} from "../models/Inventory.js";
 
 export const fetchInventory = async (req, res) => {
   try {
@@ -20,5 +24,39 @@ export const fetchById = async (req, res) => {
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+export const createInventory = async (req, res) => {
+  try {
+    const { inventory_name, cat_name, brand, qty, name, status } = req.body;
+    console.log("req.body:", req.body);
+    console.log("cat_name:", cat_name);
+    if (
+      !inventory_name ||
+      !cat_name ||
+      !brand ||
+      qty == null ||
+      !name ||
+      !status
+    )
+      return res.status(400).json({ error: "Missing required fields! ⚠️" });
+
+    const newInventoryItem = await insertInventory(
+      inventory_name,
+      cat_name,
+      brand,
+      qty,
+      name,
+      status,
+    );
+
+    res.status(201).json({
+      message: "Item inserted successfully ✅",
+      item: newInventoryItem,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
   }
 };
