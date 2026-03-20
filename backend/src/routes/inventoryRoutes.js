bbb86607e4cb00f8d3ccbd104b8863b82ecf2ff1
LEONHEIL <leozcordz@gmail.com>
2026-03-20 15:55:56 +0800

    implement put and delete request in inventory router and rename dynamic ids

--- backend/src/routes/inventoryRoutes.js
+++ backend/src/routes/inventoryRoutes.js
@@ -3,12 +3,16 @@ import {
   fetchInventory,
   fetchById,
   createInventory,
+  updateInventory,
+  deleteInventory,
 } from "../controllers/inventoryController.js";
 
 const router = Router();
 
 router.get("/inventory", fetchInventory);
-router.get("/inventory/:id", fetchById);
+router.get("/inventory/:inventory_id", fetchById);
 router.post("/inventory/", createInventory);
+router.put("/inventory/:inventory_id", updateInventory);
+router.delete("/inventory/:inventory_id", deleteInventory);
 
 export default router;
