5e2232c852feb5e1773eee7b07ebfa83e58a2d60
LEONHEIL <leozcordz@gmail.com>
2026-03-20 15:46:35 +0800

    Add urlencoded middleware to parse form data

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
