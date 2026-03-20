ce86c56e8d21b36ecd5caa244f70131692545e23
LEONHEIL <leozcordz@gmail.com>
2026-03-20 15:57:26 +0800

    implement and refactor get, post, and put request in api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -8,5 +8,59 @@ export const fetchInventory = async () => {
     return response.data;
   } catch (error) {
     console.error("Error fetching inventory:", error);
+    throw error;
+  }
+};
+
+export const fetchInventoryById = async (id) => {
+  try {
+    const response = await axios.get(`${URL}/inventory/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching inventory:", error);
+    throw error;
+  }
+};
+
+export const fetchRoom = async () => {
+  try {
+    const response = await axios.get(`${URL}/rooms`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching rooms:", error);
+    throw error;
+  }
+};
+
+export const createInventory = async (item) => {
+  try {
+    const { data } = await axios.post(`${URL}/inventory`, item);
+    return data;
+  } catch (error) {
+    console.error("Error creating item:", error);
+    throw error;
+  }
+};
+
+export const updateInventory = async (item) => {
+  try {
+    const { data } = await axios.put(
+      `${URL}/inventory/${item.inventory_id}`,
+      item,
+    );
+    return data;
+  } catch (error) {
+    console.error("Error updating item:", error);
+    throw error;
+  }
+};
+
+export const deleteInventory = async (inventory_id) => {
+  try {
+    const { data } = await axios.delete(`${URL}/inventory/${inventory_id}`);
+    return data;
+  } catch (error) {
+    console.error("Error deleting item:", error);
+    throw error;
   }
 };
