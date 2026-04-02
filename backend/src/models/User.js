eadd768bc5abc336a08ca958507013fa682887f8
Corddzz <leozcordz@gmail.com>
2026-04-03 00:23:00 +0800

    feat: add crud operations in model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,16 +1,35 @@
 import pool from "../config/connect.js";
 
-export const findUserByEmail = async (email) => {
-  const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+export const findByEmail = async (email) => {
+  const sql = "SELECT * FROM users WHERE email = ?";
+  const [result] = await pool.execute(sql, [
     email,
   ]);
-  return rows[0];
+  return result[0];
 };
 
-export const createUser = async (email, password) => {
-  const [result] = await pool.query(
+export const insert = async (email, password) => {
+  const [result] = await pool.execute(
     "INSERT INTO users (email, password) VALUES (?, ?)",
     [email, password],
   );
-  return result.insertId;
+  return result;
+};
+
+export const getAll = async () => {
+  const sql = "SELECT id, email FROM users";
+  const [result] = await pool.execute(sql);
+  return result;
+};
+
+export const update = async (id, email) => {
+  const sql = "UPDATE users SET email = ? WHERE id =?";
+  const [result] = await pool.execute(sql, [email, id]);
+  return result;
+};
+
+export const remove = async (id) => {
+  const sql = "DELETE FROM users WHERE id = ?";
+  const [result] = await pool.execute(sql, [id]);
+  return result;
 };
