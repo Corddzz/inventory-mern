6b542a7d25fd63f5d2c52fc1f5c0d13198715799
Corddzz <leozcordz@gmail.com>
2026-04-03 00:21:09 +0800

    feat: add auth and user routes in entrypoint

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from 'cookie-parser';
 import inventoryRoutes from "./src/routes/inventoryRoutes.js";
-import roomRoutes from "./src/routes/roomRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
+import userRoutes from './src/routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,16 +12,18 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*", credentials: true }));
 
 app.get("/", (req, res) => {
   console.log("Hello World!");
   res.send("Hello World!");
 });
 
-// Middlware
+// Routes
 app.use("/", inventoryRoutes);
-app.use("/", roomRoutes);
+app.use("/", authRoutes);
+app.use("/", userRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
