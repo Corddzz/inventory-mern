408ef8788eae1c0a5420ad14ebd77a71769a76c5
LEONHEIL <leozcordz@gmail.com>
2026-04-09 11:38:51 +0800

    feat: merge speceific file from pc-work

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,16 +1,34 @@
 import pool from "../config/connect.js";
 
+export const getAll = async () => {
+  const sql = "SELECT id, email, role FROM users";
+  const [result] = await pool.execute(sql);
+  return result;
+};
+
+export const findById = async (id) => {
+  const sql = "SELECT * FROM users WHERE id = ?";
+  const [rows] = await pool.execute(sql, [id]);
+  return rows[0];
+};
+
 export const findByEmail = async (email) => {
   const sql = "SELECT * FROM users WHERE email = ?";
-
   const [rows] = await pool.execute(sql, [email]);
   return rows[0];
 };
 
 export const insert = async (email, password) => {
-  const sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+  const [result] = await pool.execute(
+    "INSERT INTO users (email, password) VALUES (?, ?)",
+    [email, password],
+  );
+  return result;
+};
 
-  const [result] = await pool.execute(sql, [email, password]);
+export const update = async (id, email) => {
+  const sql = "UPDATE users SET email = ? WHERE id =?";
+  const [result] = await pool.execute(sql, [email, id]);
   return result;
 };
 
