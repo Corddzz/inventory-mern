eb1c28590e9bf2599a7e79029637a4bb86b4d845
LEONHEIL <leozcordz@gmail.com>
2026-04-19 21:33:53 +0800

    feat: add logout and getme routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { login, signUp } from "../controllers/authController.js";
+import { getMe, login, logout, signUp } from "../controllers/authController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
+router.post("/logout", verifyToken, logout);
+router.get("/me", verifyToken, getMe);
 
 export default router;
